7cc1d80d18b9b1c635d1de66bdd06ae3ab48b2fc
Guilherme Chalita <guilhermechalita@hotmail.com>
2024-02-26 16:24:48 -0300

    update secret key

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -8,9 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0+a^o87f!2og!)j)srnz5e2&rrqz(f@)h*zoffdyv8)%r+8(=i'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -137,7 +134,10 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-0+a^o87f!2og!)j)srnz5e2&rrqz(f@)h*zoffdyv8)%r+8(=i'
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
